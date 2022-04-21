53c0b7efdad12baaf1191cc8cb4d17d092bd89fa
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-04-21 17:18:18 +0200

    challenge solved! adding extra map with words and letters key - value pair!

--- src/main/java/bootstrap/DataLoader.java
+++ src/main/java/bootstrap/DataLoader.java
@@ -56,6 +56,14 @@ public class DataLoader {
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
 
+        Map<String,String > vocabulary = new HashMap<>();
+
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("SOUTH", "S");
+
         int loc = 1;
         while(true) {
             System.out.println(locations.get(loc).getDESCRIPTION());
@@ -71,6 +79,16 @@ public class DataLoader {
             System.out.println();
 
             String direction = MyScanner.getScanner().nextLine().toUpperCase(Locale.ROOT);
+            if (direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for (String word : words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
             if (exits.containsKey(direction)) {
                 loc = exits.get(direction);
             }
