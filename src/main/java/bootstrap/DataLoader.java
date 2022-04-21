31733789c58aca77b65838dc61a81176d0cf4d45
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-04-21 16:45:21 +0200

    add challenge.txt and creat a method to solve

--- src/main/java/bootstrap/DataLoader.java
+++ src/main/java/bootstrap/DataLoader.java
@@ -4,6 +4,7 @@ import model.Location;
 import util.MyPrinter;
 import util.MyScanner;
 
+import javax.swing.*;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -16,7 +17,68 @@ public class DataLoader {
     String text = "Choose a num between 0-5: ";
 
     public void loadTestData() {
-        testProgramRunning();
+        // codeBeforeRefactor();
+        // automaticTestRunner();
+        // testProgramRunning();
+        showResultOfAdventureGameChallenge();
+    }
+
+    private void showResultOfAdventureGameChallenge() {
+        // Challenge is:
+
+        // Change the program to allow players to type full words, or phrases, then move to the
+        // correct location based upon their input.
+        // The player should be able to type commands such as "Go West", "run South", or just "East"
+        // and the program will move to the appropriate location if there is one.  As at present, an
+        // attempt to move in an invalid direction should print a message and remain in the same place.
+        //
+        // Single letter commands (N, W, S, E, Q) should still be available.
+
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
+        locations.put(2, new Location(2, "You are at the top of a hill"));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(4, "You are in a valley beside a stream"));
+        locations.put(5, new Location(5, "You are in the forest"));
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 1);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+
+        int loc = 1;
+        while(true) {
+            System.out.println(locations.get(loc).getDESCRIPTION());
+            if (loc == 0) {
+                break;
+            }
+            Map<String, Integer> exits = locations.get(loc).getEXITS();
+            new MyPrinter().print(newText);
+            System.out.print("Available exits are: ");
+            for (String exit : exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = MyScanner.getScanner().nextLine().toUpperCase(Locale.ROOT);
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            }
+            else {
+                System.out.println("You cannot go in that direction");
+            }
+        }
+
     }
 
     private void testProgramRunning() {
@@ -98,6 +160,43 @@ public class DataLoader {
             if(!locationsBeforeRefactor.containsKey(loc)) {
                 System.out.println("You cannot go in that direction");
             }
+        }
+    }
+
+    private void automaticTestRunner() {
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
+        locations.put(2, new Location(2, "You are at the top of a hill"));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(4, "You are in a valley beside a stream"));
+        locations.put(5, new Location(5, "You are in the forest"));
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 1);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+
+
+        String[] road = "You are standing at the end of a road before a small brick building".split(" ");
+        for (String i : road) {
+            System.out.println(i);
+        }
+
+        System.out.println("==================================");
+
+        String[] building = "You are inside a building, a well house for a small spring".split(", ");
+        for (String i : building) {
+            System.out.println(i);
         }
     }
 }
